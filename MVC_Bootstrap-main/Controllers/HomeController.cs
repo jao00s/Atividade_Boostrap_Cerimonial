31d6eb53ab28caf31891e0368ca45a6d44e4484f
jao00s <joaofcojau@gmail.com>
2026-09-09 10:25:32 -0300

    atividade nando

---
+++ MVC_Bootstrap-main/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using CerimonialCasamentos.Models;
+
+namespace CerimonialCasamentos.Controllers;
+
+public class HomeController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Sobre()
+    {
+        return View();
+    }
+
+    public IActionResult Servicos()
+    {
+        return View();
+    }
+
+    public IActionResult Depoimentos()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
